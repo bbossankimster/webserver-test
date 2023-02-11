ea5c19ebc663c8b6c27dda26c7c5ddb5a2e8a7a5
Andrey N <nepshekuev.a.88@gmail.com>
2023-02-12 02:28:51 +0300

    refactor: add exceptions to add_attachment()

--- apps/webserver_test/utils/mail_attachment.py
+++ apps/webserver_test/utils/mail_attachment.py
@@ -1,6 +1,7 @@
 from email.mime.base import MIMEBase
 from email import encoders
 from email.mime.text import MIMEText
+import io
 
 
 def attach_scv(msg, csv_buffer, filename):
@@ -11,13 +12,20 @@ def attach_scv(msg, csv_buffer, filename):
     msg.attach(attachment)
 
 
-def add_attachment(message, attachments):
+def add_attachment(message, attachments: list):
+    REQUIRED_KEYS = ["type", "content"]
     for attchmnt in attachments:
         # type: plain, html, csv
-        if attchmnt["type"] == "text":
-            message.attach(MIMEText(attchmnt["content"], 'plain'))
-        if attchmnt["type"] == "html":
-            part = MIMEText(attchmnt["content"], attchmnt["type"])
-            message.attach(part)
-        if attchmnt["type"] == "csv":
-            attach_scv(message, attchmnt["content"], attchmnt["name"])
+        if set(attchmnt.keys()).intersection(set(REQUIRED_KEYS)) == set(REQUIRED_KEYS):
+            if attchmnt["type"] == "text":
+                message.attach(MIMEText(attchmnt["content"], 'plain'))
+            if attchmnt["type"] == "html":
+                message.attach(MIMEText(attchmnt["content"], attchmnt["type"]))
+            if attchmnt["type"] == "csv":
+                csv_data = attchmnt["content"]
+                if type(csv_data) is not io.StringIO:
+                    raise TypeError("EXCEPTION ERROR: invalid csv type {}".format(type(csv_data)))
+                else:
+                    attach_scv(message, attchmnt["content"], attchmnt.get("name", "noname.csv"))
+        else:
+            raise ValueError("EXCEPTION ERROR: wrong keys in attachment {}".format(attchmnt))
