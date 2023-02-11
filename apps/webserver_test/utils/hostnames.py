3ed14525f955f1942184b02787244ff554c83fd2
Andrey N <nepshekuev.a.88@gmail.com>
2023-02-12 02:29:59 +0300

    refactor: replace exec.nslookup to socket.gethostbyname_ex

--- apps/webserver_test/utils/hostnames.py
+++ apps/webserver_test/utils/hostnames.py
@@ -1,21 +1,13 @@
-import subprocess
-import re
-import platform
+import socket
 
 
-NSLOOKUP_TMPLT = "nslookup {} | grep Address | grep -v '#53'"
-if platform.system() == "Linux":
-    WIN_LINUX_SSH_TMPLT = r'{cmd}'
-else:
-    WIN_LINUX_SSH_TMPLT = r'cd C:\Users\1\Downloads\putty-0.73-ru-17-portable\PuTTY PORTABLE & plink netnut "{cmd}"'
-
-
-def hostname_to_ips(host_domain):
-    cmd = WIN_LINUX_SSH_TMPLT.format(cmd=NSLOOKUP_TMPLT.format(host_domain))
-    exec = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding='utf-8')
-    out = exec.stdout
-    err = exec.stderr
-    ips = re.findall(r'\d+.\d+.\d+.\d+', out)
-    if ips == []:
-        raise ValueError("EXCEPTION ERROR: nslookup error for {}, out: {}, regexp_ips: {}".format(host_domain, out, ips))
-    return sorted(ips)
+def hostname_to_ips(domain):
+    try:
+        addr_info = socket.gethostbyname_ex(domain)
+    except socket.gaierror:
+        raise ValueError("EXCEPTION ERROR: can not resolve {}".format(domain))
+    else:
+        ips = addr_info[2]
+        if ips == []:
+            raise ValueError("EXCEPTION ERROR: can not resolve {}, got empty alias list".format(domain))
+        return sorted(ips)
