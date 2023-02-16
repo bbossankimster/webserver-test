f703a686fd8f597e57795cc81cb2cc49bfae2307
Andrey N <nepshekuev.a.88@gmail.com>
2023-02-16 05:03:12 +0300

    fix: replace 'percent' symbol in add_arguments

--- apps/start_webservertest.py
+++ apps/start_webservertest.py
@@ -15,7 +15,7 @@ def add_arguments(parser, conf):
     parser.add_argument('mode', type=str, choices=['simple', 'with_logs_gathering', 'with_alerts_sending'])
     parser.add_argument('-d', '--host_domains', default="example1.site.com", type=str, help='separated by commas, example: -d example1.site.com,example2.site.com')
     parser.add_argument('-a', '--web_applications', action=AppsAction, default=conf.services, type=str, help='separated by commas, example: -a reports,support,analytics')
-    parser.add_argument('-t', '--err_threshold', default=10, type=float, help='errors threshold (%) for services, example: -t 15')
+    parser.add_argument('-t', '--err_threshold', default=10, type=float, help=r'errors threshold (percent)) for services, example: -t 15')
     # mail alerts options
     parser.add_argument('-e', '--send_errors_to', default=conf.send_to_errors, type=str, help='send errors report to, example: -e andrey@example.com')
     parser.add_argument('-n', '--send_no_errors_to', default=conf.send_to_no_errors, type=str, help='send no_errors report to, example: -n no_errors@example.com')
@@ -52,6 +52,7 @@ if __name__ == "__main__":
     conf = WtestSettings("wtest.conf")
     add_arguments(parser, conf)
     args = parser.parse_args()
+    error_code = None
     if args.update_settings:
         conf.input_from_console()
     if not conf.is_default:
